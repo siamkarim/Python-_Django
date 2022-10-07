aa09ca47c8eca654fb88397fbb60ce6332fddfe1
siamkarim <siamhossain518@gmail.com>
2022-10-07 21:38:41 +0600

    reate & Works Template Inheritance in Django

--- learnDjango/Deep_learning/urls.py
+++ learnDjango/Deep_learning/urls.py
@@ -3,5 +3,5 @@ from.import views
 urlpatterns = [
    
  
-    path('dp/',views.deep),
+    path('deep/',views.deep),
 ]
--- learnDjango/Deep_learning/views.py
+++ learnDjango/Deep_learning/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def deep (request):
-    return render (request,'deep_learnig/deep_learnig.html')
+    return render (request,'deep_learning/deep_learnig.html')

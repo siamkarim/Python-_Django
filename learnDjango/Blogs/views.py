5a06c25077ac9e37609ff598365005816f9844f0
siamkarim <siamhossain518@gmail.com>
2022-10-08 01:24:30 +0600

    Create & Works Template Inheritance in Django

--- learnDjango/Blogs/views.py
+++ learnDjango/Blogs/views.py
@@ -3,6 +3,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def blog1(request):
-    siam ="This is blog section ,we are working here"
-    fil = {'var':siam}
-    return render(request,'blog/blog.html',context = fil)
+  
+    return render(request,'blog/blog.html')

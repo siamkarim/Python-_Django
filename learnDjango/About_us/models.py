d4586b6bae5096ef22b134b650a504e3d6ae3edb
siamkarim <siamhossain518@gmail.com>
2022-10-08 14:14:03 +0600

    model create MakeMigrations & Migrate Works

--- learnDjango/About_us/models.py
+++ learnDjango/About_us/models.py
@@ -1,3 +1,8 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+class teacher(models.Model):
+    tid = models.IntegerField()
+    tname = models.CharField(max_length =40)
+    tmail = models.EmailField(max_length =30 )

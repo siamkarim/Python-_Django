00f034585f713385a230b01a3c9831c83b4b45e4
siamkarim <siamhossain518@gmail.com>
2022-10-10 00:43:32 +0600

    Style in Form, Match Password & RePassword

--- learnDjango/Blogs/forms.py
+++ learnDjango/Blogs/forms.py
@@ -1,11 +1,19 @@
 from django import forms
-
+from django.core import validators
 
 class TeacherRegistrations(forms.Form):
     first_name = forms.CharField(label='Enter Your First Name',label_suffix=' ')
     last_name = forms.CharField( initial='Hossain')
     Email = forms.EmailField(initial='siam@gmail.com', disabled=True )
     password = forms.CharField(widget= forms.PasswordInput)
+    repassword = forms.CharField(widget= forms.PasswordInput)
     Textarea = forms.CharField(widget=forms.Textarea)
     file = forms.CharField(widget=forms.FileInput)
     checkbox = forms.CharField(widget=forms.CheckboxInput )
+    
+    def clean(self):
+        cleaned_data = super().clean()
+        rightpassword = self.cleaned_data['password']
+        wrongpassword = self.cleaned_data['repassword']
+        if rightpassword != wrongpassword:
+            raise forms.ValidationError('password does not match')
--- learnDjango/Blogs/views.py
+++ learnDjango/Blogs/views.py
@@ -8,6 +8,17 @@ def blog1(request):
     return render(request,'blog/blog.html')
 
 def showFormsData(request):
-    fm =  TeacherRegistrations()
+    if request.method == 'POST':
+      fm =  TeacherRegistrations(request.POST)
+      if fm.is_valid():
+       print(fm.cleaned_data['password'])
+       print(fm.cleaned_data['repassword'])
+      
+      
+ 
+    else:
+        fm =  TeacherRegistrations()
+        print('this is get statement')
+
    
     return render(request, 'blog/form.html',{'form': fm})
